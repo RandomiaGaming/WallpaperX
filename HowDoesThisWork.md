db1b01a87842148ca8d74e74f136a09b16a0d78c
RandomiaGaming <randomiagaming@gmail.com>
2025-05-09 21:02:39 -0700

    Version 2.0 way more stable.

---
+++ HowDoesThisWork.md
@@ -0,0 +1,43 @@
+﻿The desktop wallpaper, desktop icons, taskbar and many other system UI elements are all powered by explorer.exe.
+The window in charge of rendering the wallpaper and desktop icons is Progman.
+Progman has two modes for rendering the wallpaper, normal mode and layered mode.
+In normal mode the window structure looks like this:
+Desktop (CSRSS.exe)
+---> Other Apps With Windows Like
+---> Settings
+---> Google Chrome
+---> Steam
+---> Progman (Explorer.exe)
+-------> SHELLDLL_DefView (Explorer.exe)
+-----------> SysListView32 (Explorer.exe)
+When in normal mode SysListView32 draws both the desktop icons and the wallpaper.
+So you cannot place a window in between these two layers since they aren't separate layers at all but one single window.
+But we can send the WM_SWITCH_TO_LAYERED_WALLPAPER_MODE message to Progman (sometimes called WM_SPAWN_WORKERW) with message code 0x052C.
+After this switch the window structure will look like this:
+Desktop (CSRSS.exe)
+---> Other Apps With Windows Like
+---> Settings
+---> Google Chrome
+---> Steam
+---> WorkerW
+-------> SHELLDLL_DefView (Explorer.exe)
+-----------> SysListView32 (Explorer.exe)
+---> WorkerW
+---> Progman (Explorer.exe)
+Now we can see Explorer.exe has shifted to layered wallpaper mode.
+In this mode we have two new windows with the class WorkerW.
+To distinguish them we will give them the following names.
+The WorkerW with SHELLDLL_DefView and SysListView32 as children we will call IconsWorkerW.
+And the WorkerW with no children we will call WallpaperWorkerW.
+IconsWorkerW renders the desktop icons and handles user input when interacting with the desktop.
+WallpaperWorkerW is where the wallpaper is drawn.
+To draw custom graphics to the wallpaper we can simply create a child window under WallpaperWorkerW.
+Now it is important to note if our window gets destroyed while it is a child of WallpaperWorkerW it will send a WM_PARENTNOTIFY message to WallpaperWorkerW with WParam = WM_DESTROY.
+If this happens WallpaperWorkerW will also destroy itself.
+This is not what we want because Explorer.exe will still be in layered wallpaper mode even if WallpaperWorkerW is destroyed.
+As such it will ignore WM_SWITCH_TO_LAYERED_WALLPAPER_MODE requests since it thinks we are already in layered wallpaper mode.
+But with WallpaperWorkerW gone it becomes impossible to use layered wallpaper mode.
+As such we should always change parents to NULL before destroying a window or block the WM_PARENTNOTIFY message.
+Additional note, Explorer.exe will take windows whose z-index is behind Progman and simply place them one layer in front of Progman.
+This behavior is so developers can send their window to the back with HWND_BOTTOM without sending it behind the desktop wallpaper by mistake.
+Additionally in layered wallpaper mode Explorer.exe will move windows placed behind IconsWorkerW to one layer in front of IconsWorkerW.
--- Program.cs
+++ Program.cs
@@ -1,106 +1,90 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows.Forms;
 using CefSharp;
 using CefSharp.WinForms;
+using Microsoft.Win32;
+using Microsoft.VisualBasic;
 
-public static class Program
+namespace WallpaperX
 {
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
-    private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam, uint fuFlags, uint uTimeout, out IntPtr lpdwResult);
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-
-    private static IntPtr GetWorkerW()
+    public static class PInvoke
     {
-        IntPtr workerW = IntPtr.Zero;
-        EnumWindows((topHandle, lParam) =>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        public const uint WM_SWITCH_TO_LAYERED_WALLPAPER_MODE = 0x052C;
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint wMsg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        public const int SPI_SETDESKWALLPAPER = 0x0014;
+        public const int SPIF_SENDCHANGE = 0x02;
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
         {
-            IntPtr shellView = FindWindowEx(topHandle, IntPtr.Zero, "SHELLDLL_DefView", null);
-            if (shellView != IntPtr.Zero)
-            {
-                workerW = FindWindowEx(IntPtr.Zero, topHandle, "WorkerW", null);
-                return false;
-            }
-            return true;
-        }, IntPtr.Zero);
-
-        if (workerW == IntPtr.Zero)
-        {
-            IntPtr progman = FindWindow("Progman", null);
-            SendMessageTimeout(progman, 0x052C, IntPtr.Zero, IntPtr.Zero, 0, 1000, out _);
-
-            EnumWindows((topHandle, lParam) =>
-            {
-                IntPtr shellView = FindWindowEx(topHandle, IntPtr.Zero, "SHELLDLL_DefView", null);
-                if (shellView != IntPtr.Zero)
-                {
-                    workerW = FindWindowEx(IntPtr.Zero, topHandle, "WorkerW", null);
-                    return false;
-                }
-                return true;
-            }, IntPtr.Zero);
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
         }
-
-        if (workerW == IntPtr.Zero)
-        {
-            foreach (var proc in Process.GetProcessesByName("explorer"))
-            {
-                try { proc.Kill(); } catch { }
-            }
-            Thread.Sleep(1000);
-
-            IntPtr progman = FindWindow("Progman", null);
-            SendMessageTimeout(progman, 0x052C, IntPtr.Zero, IntPtr.Zero, 0, 1000, out _);
-
-            EnumWindows((topHandle, lParam) =>
-            {
-                IntPtr shellView = FindWindowEx(topHandle, IntPtr.Zero, "SHELLDLL_DefView", null);
-                if (shellView != IntPtr.Zero)
-                {
-                    workerW = FindWindowEx(IntPtr.Zero, topHandle, "WorkerW", null);
-                    return false;
-                }
-                return true;
-            }, IntPtr.Zero);
-        }
-
-        if (workerW == IntPtr.Zero)
-        {
-            throw new Exception("WorkerW is cooked :(");
-        }
-        return workerW;
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+        public const uint GW_HWNDNEXT = 2;
+        public const uint GW_HWNDPREV = 3;
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
     }
-
-    public partial class WallpaperForm : Form
+    public partial class WallpaperXForm : Form
     {
-        private ChromiumWebBrowser browser;
-        private NotifyIcon trayIcon;
-        public WallpaperForm(string url)
+        private ChromiumWebBrowser browser = null;
+        private NotifyIcon trayIcon = null;
+        private IntPtr wallpaperWorkerW = IntPtr.Zero;
+        public WallpaperXForm()
         {
-            CefSettings settings = new CefSettings();
-            settings.CefCommandLineArgs.Add("autoplay-policy", "no-user-gesture-required");
-            Cef.Initialize(settings);
-
-            browser = new ChromiumWebBrowser()
+            // Init wallpaper window layering
+            IntPtr progman = PInvoke.FindWindow("Progman", null);
+            PInvoke.SendMessage(progman, PInvoke.WM_SWITCH_TO_LAYERED_WALLPAPER_MODE, IntPtr.Zero, IntPtr.Zero);
+            IntPtr iconsWorkerW = IntPtr.Zero;
+            PInvoke.EnumWindows((enumHwnd, lParam) =>
             {
-                Dock = DockStyle.Fill
-            };
-            this.Controls.Add(browser);
+                IntPtr shellDllDefView = PInvoke.FindWindowEx(enumHwnd, IntPtr.Zero, "SHELLDLL_DefView", null);
+                if (shellDllDefView != IntPtr.Zero)
+                {
+                    iconsWorkerW = enumHwnd;
+                    return false;
+                }
+                return true;
+            }, IntPtr.Zero);
+            if (iconsWorkerW == IntPtr.Zero)
+            {
+                throw new Exception("IconWorkerW could not be found. Try restarting your PC.");
+            }
+            wallpaperWorkerW = PInvoke.GetWindow(iconsWorkerW, PInvoke.GW_HWNDNEXT);
+            if (wallpaperWorkerW != PInvoke.GetWindow(progman, PInvoke.GW_HWNDPREV) || wallpaperWorkerW == IntPtr.Zero)
+            {
+                throw new Exception("WallpaperWorkerW could not be found. Try restarting your PC.");
+            }
+            PInvoke.SetParent(this.Handle, wallpaperWorkerW);
 
-            browser.Load(url);
+            // Init window
+            this.Text = "WallpaperX Renderrer";
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.Manual;
+            PInvoke.RECT clientRect;
+            PInvoke.GetClientRect(wallpaperWorkerW, out clientRect);
+            this.Location = new Point(clientRect.Left, clientRect.Top);
+            this.Size = new Size(clientRect.Right - clientRect.Left, clientRect.Bottom - clientRect.Top);
 
+            // Init systray icon
             ContextMenuStrip trayMenu = new ContextMenuStrip();
             trayMenu.Items.Add("Open Devtools", null, (object sender, EventArgs e) =>
             {
@@ -109,18 +93,28 @@ public static class Program
                     browser.GetBrowser().ShowDevTools();
                 }
             });
+            trayMenu.Items.Add("Set Wallpaper Url", null, (object sender, EventArgs e) =>
+            {
+                if (!File.Exists("wallpaper_url.txt"))
+                {
+                    File.WriteAllText("wallpaper_url.txt", "file:///" + Path.GetFullPath("./example/wallpaper.html").Replace("\\", "/"));
+                }
+                string url = File.ReadAllText("wallpaper_url.txt");
+                string newUrl = Interaction.InputBox("Enter new wallpaper url:", "", url);
+                if (newUrl != null && newUrl != "")
+                {
+                    File.WriteAllText("wallpaper_url.txt", newUrl);
+                    this.ReloadWallpaper();
+                }
+            });
             trayMenu.Items.Add("Reload", null, (object sender, EventArgs e) =>
             {
-                if (browser.IsBrowserInitialized)
-                {
-                    browser.Load(url);
-                }
+                this.ReloadWallpaper();
             });
             trayMenu.Items.Add("Quit", null, (object sender, EventArgs e) =>
             {
                 this.Close();
             });
-
             trayIcon = new NotifyIcon
             {
                 Text = "WallpaperX",
@@ -128,36 +122,64 @@ public static class Program
                 ContextMenuStrip = trayMenu,
                 Visible = true
             };
+
+            // Init chromium browser
+            CefSettings settings = new CefSettings();
+            settings.CefCommandLineArgs.Add("autoplay-policy", "no-user-gesture-required");
+            Cef.Initialize(settings);
+            browser = new ChromiumWebBrowser()
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(browser);
+
+            ReloadWallpaper();
         }
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
+            PInvoke.SetParent(this.Handle, IntPtr.Zero);
+            string originalWallpaper = (string)Registry.GetValue(@"HKEY_CURRENT_USER\Control Panel\Desktop", "WallPaper", "");
+            PInvoke.SystemParametersInfo(PInvoke.SPI_SETDESKWALLPAPER, 0, originalWallpaper, PInvoke.SPIF_SENDCHANGE);
+
             Cef.Shutdown();
+
             trayIcon.Visible = false;
             trayIcon.Dispose();
+
             base.OnFormClosing(e);
         }
-    }
-
-    public static void Main()
-    {
-        if (!File.Exists("wallpaper_url.txt"))
+        public void ReloadWallpaper()
         {
-            File.WriteAllText("wallpaper_url.txt", Path.GetFullPath("./example/wallpaper.html"));
+            if (!File.Exists("wallpaper_url.txt"))
+            {
+                File.WriteAllText("wallpaper_url.txt", "file:///" + Path.GetFullPath("./example/wallpaper.html").Replace("\\", "/"));
+            }
+            string url = File.ReadAllText("wallpaper_url.txt");
+            if (browser.IsBrowserInitialized)
+            {
+                browser.Load(url);
+            }
+            else
+            {
+                browser.IsBrowserInitializedChanged += (object sender, EventArgs e) =>
+                {
+                    browser.Load(url);
+                };
+            }
         }
-
-        string wallpaperUrl = File.ReadAllText("wallpaper_url.txt");
-
-        IntPtr workerW = GetWorkerW();
-
-        WallpaperForm wallpaperForm = new WallpaperForm(wallpaperUrl)
+    }
+    public static class Program
+    {
+        public static void Main()
         {
-            FormBorderStyle = FormBorderStyle.None,
-            Width = 1920,
-            Height = 1080,
-        };
-
-        SetParent(wallpaperForm.Handle, workerW);
-
-        Application.Run(wallpaperForm);
+            try
+            {
+                Application.Run(new WallpaperXForm());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "WallpaperX - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
